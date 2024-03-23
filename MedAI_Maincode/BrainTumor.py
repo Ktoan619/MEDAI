1e7cae7b147d2a04b3dc56f0486d5cd6d94ec789
Ktoan619 <133044715+Ktoan619@users.noreply.github.com>
2024-03-23 19:41:00 +0700

    Update BrainTumor.py
    added pipe added, create tab function

--- MedAI_Maincode/BrainTumor.py
+++ MedAI_Maincode/BrainTumor.py
@@ -1,6 +1,13 @@
-def generateBrainTumorAI():
-  pipe = pipeline("object-detection", model="DunnBC22/yolos-tiny-Brain_Tumor_Detection")
-def Processing([Image, pipe]):
+from PIL import Image
+from transformers import pipeline
+import datasets
+
+from datasets import load_dataset
+
+data = load_dataset("Francesco/brain-tumor-m2pbp")
+pipe = pipeline("object-detection", model="DunnBC22/yolos-tiny-Brain_Tumor_Detection")
+
+def Processing(Image, pipe) :
   Color_List = ["purple", "yellow", "blue"]
 
   data = pipe(Image)
@@ -18,3 +25,15 @@ def Processing([Image, pipe]):
     draw.rectangle([xmin, ymin, xmax, ymax], outline= Color_List[Num_of_Label], width=2)
 
   return Image
+  
+  def create_brain_tumor_detec() :
+  with gr.Blocks() as Brain_Tumor_Detect:
+      gr.Markdown("Cùng kiểm tra xem bạn có khối u não không nào =)))")
+      with gr.Row():
+
+          inp = gr.Image(type = 'pil')
+          out = gr.Image(type = 'pil')
+
+      btn = gr.Button("Run")
+      btn.click(fn=Processing, inputs= inp, outputs=out)
+  return Brain_Tumor_Detect
