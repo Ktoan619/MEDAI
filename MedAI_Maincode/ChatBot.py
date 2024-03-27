d00ff125e1b6b0779500fee41ea057aaea6b82df
Ktoan619 <133044715+Ktoan619@users.noreply.github.com>
2024-03-27 16:44:22 +0700

    Update ChatBot.py

--- MedAI_Maincode/ChatBot.py
+++ MedAI_Maincode/ChatBot.py
@@ -1,77 +1,35 @@
 import gradio as gr
-import os
-import time
-import openai
-#print(openai.VERSION)
 from openai import OpenAI
 
-model = "gpt-3.5-turbo-1106"
-#openai.api_key = "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"
+secret_key="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"
+MODEL = "gpt-3.5-turbo"
+client = OpenAI(api_key =secret_key)
 
-os.environ["OPENAI_API_KEY"] = "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"
+def create_Chatbot_tab():
+    initial_messages=[{"role": "system", "content": '''Bạn là bác sĩ và trợ lý y tế.
+                                            Nếu người dùng hỏi về bất kỳ bệnh nào, hãy cho họ biết thông tin về bệnh đó.
+                                            Phải hướng cuộc trò chuyện đến chủ đề y tế, sức khỏe khi người dùng hỏi về các vấn đề bên ngoài và nói với họ rằng bạn chỉ có thể tư vấn về các vấn đề liên quan đến sức khỏe.
+                                            Cố gắng hỏi càng nhiều thông tin về triệu chứng bệnh (cung cấp cho bệnh nhân một số câu hỏi về triệu chứng bệnh đó) và cung cấp cho bệnh nhân một số thông tin về bệnh liên quan và chẩn đoán cho họ.
+                                            TUYỆT ĐỐI ĐỪNG NÓI NHỮNG CÂU NHƯ TÔI KHÔNG PHẢI LÀ BÁC SĨ HAY không thể thay thế cho sự tư vấn y tế chuyên nghiệp.
+                                            '''}]
+    def respond(message,chatHistory):
+        messages = initial_messages
+        for human, assistant in chatHistory:
+            messages.append({"role": "user", "content": human })
+            messages.append({"role": "assistant", "content":assistant})
+        messages.append({"role": "user", "content": message})
 
-#openai_client = openai.OpenAI(api_key=get_openai_key())
-openai_client = OpenAI()
-openai_threads = openai_client.beta.threads
-openai_assistants = openai_client.beta.assistants
-def create_assistant():
-  assistant = openai_assistants.create(
-    name="Bác sĩ",
-    instructions="Bạn là một trợ lý tư vấn về sức khỏe. Bạn sẽ giúp người dùng giải thích các vấn đề sức khỏe và đưa ra giải pháp.",
-    tools=[],
-    model=model,
-  )
+        
+        response = client.chat.completions.create(
+          model=MODEL,
+          messages=messages,
+          max_tokens=500,
+          seed=2023,
+          # stop=,
+          temperature=0,
+        )
+        answer = response.choices[0].message.content
+        return answer
 
-  return assistant
-
-def ask_assistant(user_question, thread, assistant):
-  # Pass in the user question into the existing thread
-  openai_threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content=user_question,
-  )
-  #
-  # Use runs to wait for the assistant response
-  run = openai_threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-    instructions=f'Lưu ý rằng người dùng không có kiến thức về y học, đặc biệt là vấn đề sức khỏe nên hãy giải thích câu trả lời một cách chi tiết, đơn giản và chính xác.Giới hạn là 300 tokens'
-  )
-
-  is_running = True
-  while is_running:
-    run_status = openai_threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-    is_running = run_status.status != "completed"
-    time.sleep(1)
-
-  return run
-
-def assistant_response(thread, run):
-  # Get the messages list from the thread
-  # history
-  messages = openai_threads.messages.list(thread_id=thread.id)
-  # print(messages.data)
-  # Get the last message for the current run
-  last_message = [message for message in messages.data if message.run_id == run.id and message.role == "assistant"][-1]
-  # If an assistant message is found, print it
-  if last_message:
-    response = f"[Bs.Hera]: {last_message.content[0].text.value}"
-  else:
-    response = f"[Bs.Hera]: Xin lỗi, Tôi không chắc rằng có thể trả lời câu hỏi đó. Bạn có thể hỏi một câu khác được không?"
-  return response
-assistant = create_assistant()
-thread = openai_threads.create()
-
-def gpt_response(message,history):
-
-  user_question = message
-  run = ask_assistant(user_question, thread, assistant)
-  response = assistant_response(thread, run)
-  return response
-def create_chatbot_tab():
-  demo = gr.ChatInterface(fn=gpt_response, examples=["sùi mào gà", "đau bụng", "đau đầu", "mụn"], title="Bs.Hera",
-                          description = "Trợ lý ảo - tư vấn sức khỏe", theme = "soft", submit_btn = "Gửi", retry_btn = "Thử lại",
-                          undo_btn = "Quay lại", clear_btn = "Xóa toàn bộ", stop_btn = "Tạm dừng")
-  #def launch_interface:
-  return demo
+    Chatbot_tab = gr.ChatInterface(fn=respond, examples=["Tôi bị đau dạ dày mạn tính vừa rồi tôi ăn xoài chua,tôi nên làm gì đây?", "Tôi khó thở vì covid tôi nên làm gì đây", "Tôi bị đau dạ dày lâu năm"], title="Trợ lý sức khỏe trực tuyến",fill_height=True)
+    return Chatbot_tab
